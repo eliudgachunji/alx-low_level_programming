7c05449ba414582378d4b539d3f8c3449cf88bb0
Gachunji <eliud.gachunji@gmail.com>
2022-06-02 05:58:56 -0700

    Printing a string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/**main - 
+/**
+ * main - prints the string using printf function
+ * Description - with proper grammar, but the outcome
+ * is a piece of art,
+ * Return: 0
  */
-
-int main (void)
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n")
-
-    return (0)
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0)
 }

45bb7090b3b9f58fcd66de96fa360fa2cd989c80
Gachunji <eliud.gachunji@gmail.com>
2022-06-02 05:06:13 -0700

    Script that prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/** 
+ * main - Prints the string "Programming is like buiding a multilingual puzzle" in the puts function
+ *
+ * Return (Always 0)
+ */
 
-int main (void)
+
+
+int main(void)
 {
-  puts("/"Programming is like building a multilingual puzzle);
+    puts("\"Programming is like building a multilingual puzzle");
 
-  return (0)
+    return (0);
 }

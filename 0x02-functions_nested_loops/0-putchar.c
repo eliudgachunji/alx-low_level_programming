a7c8c42785e6dde02be340420b1e585f31eac4f6
Gachunji <eliud.gachunji@gmail.com>
2022-06-07 16:50:03 -0700

    Task 0-2 Corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - program that prints _putchar
+ * main - program that prints _putchar followed by a new line
  *
  * Return: 0
  */
 int main(void)
 {
-printf("_putchar");
+printf("_putchar\n");
 
 return (0);
 }

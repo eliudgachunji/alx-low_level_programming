02a4b4bbbb03e06f8391a5607e678f4c18dd3709
Gachunji <eliud.gachunji@gmail.com>
2022-06-07 18:33:11 -0700

    Printing the absolute value of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit -prints the last digit of a number
+ *@n: number in question * Return: the last digit of the number
+ *Return: the last digit of the number
+ */
+
+int print_last_digit(int n)
+
+{
+int last_digit = n % 10;
+
+if (last_digit < 0)
+last_digit *= -1;
+
+_putchar(last_digit + '0');
+
+return (last_digit);
+
+}

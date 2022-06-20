9b061d615f27af8e2ff6656be56f2ac339f7d42c
Nganga <eliud.gachunji@gmail.com>
2022-06-20 07:05:08 -0700

    Function that locates a substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 int _putchar(char c);

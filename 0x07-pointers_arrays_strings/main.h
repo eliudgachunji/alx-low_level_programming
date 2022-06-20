74b41be3d90c6de4bae3daa57890fb437da6c74d
Nganga <eliud.gachunji@gmail.com>
2022-06-20 06:27:30 -0700

    gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);

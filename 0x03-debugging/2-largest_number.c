8476dfd0dc807675723f3da1b4160e77073f7cd6
Nganga <eliud.gachunji@gmail.com>
2022-06-21 23:12:30 -0700

    Finding the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,30 +13,27 @@ int largest_number(int a, int b, int c)
 {
 
 int largest;
-
+largest = largest_number(a, b, c);
 if (a > b && b > c)
 
 {
 
-largest = a;
+printf("%d is the largest number\n", largest);
 
 }
 else if (b > a && a > c)
 
 {
 
-largest = b;
+printf("%d is the largest number\n", largest);
 
 }
 
 else
 
 {
-
-largest = c;
-
+printf("%d is the largest number", largest);
 }
-
 
 return (largest);
 }

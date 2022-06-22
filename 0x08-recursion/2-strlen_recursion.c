fc573f1e41f5e374d67a1b84d4cd139694819df5
Nganga <eliud.gachunji@gmail.com>
2022-06-22 01:35:59 -0700

    Length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - returns the lenght of a string
+ * @s: string in question
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+
+{
+
+int len = 0;
+
+if (*s)
+{
+
+len++;
+len += _strlen_recursion(s + 1);
+
+}
+return (len);
+}

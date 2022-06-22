6c68b278a3ba167bae9de8abdb220500503af806
Nganga <eliud.gachunji@gmail.com>
2022-06-22 04:39:53 -0700

    Compares two strings to see if they are identical

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,9 @@ int _strlen_recursion(char *s);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
 
 #endif

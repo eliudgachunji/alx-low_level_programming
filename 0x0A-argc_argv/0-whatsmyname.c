483fef714fc31fb08a0a77dbb07739cb1519e868
Nganga <eliud.gachunji@gmail.com>
2022-06-24 04:40:53 -0700

    A program that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- prints its name
+ * @argc: number of command line argumets
+ * @argv: name of array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+printf("%s\n", argv[argc * 0]);
+
+return (0);
+
+
+}

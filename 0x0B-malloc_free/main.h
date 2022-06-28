51f5c42e958aba623b2f2ab38933db313ad0d185
Nganga <eliud.gachunji@gmail.com>
2022-06-28 13:26:29 -0700

    splits a string into words

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,6 @@ char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif

22526abc1ccc84db50341ccf04c24e6762a03e9c
Nganga <eliud.gachunji@gmail.com>
2022-06-30 02:37:53 -0700

    Setting up directory for tasks ahead

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+
+ * main - program that prints _putchar followed by a new line
+
+ * Return: 0
+
+ */
+
+
+
+int main(void)
+  
+
+  
+{
+  
+
+  
+  printf("_putchar\n");
+  
+
+  
+  return (0);
+  
+
+  
